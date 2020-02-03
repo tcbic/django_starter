6ee01cd09824fe0984a326423f1647d8e80fa9e9
Taylor <bickell.taylor@gmail.com>
2020-02-02 21:20:19 -0800

    Changes made for environment variables.

--- project/settings.py
+++ project/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.0/ref/settings/
 """
-
+from decouple import config
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -21,13 +21,13 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
-DEBUG = os.environ.get('DEBUG_VALUE')
+DEBUG = config('DEBUG_VALUE', cast=bool)
 
-ALLOWED_HOSTS = ['sampleblogapp.herokuapp.com']
+ALLOWED_HOSTS = []
 
 
 # Application definition
